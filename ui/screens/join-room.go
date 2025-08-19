c91d52f64a8416087bed8b07fa837e6b69d499bd
deoxyimran <imransarkar974@gmail.com>
2025-08-19 12:54:08 +0600

    Create device selector, gioview dropdown test.

--- ui/screens/join-room.go
+++ ui/screens/join-room.go
@@ -1,6 +1,7 @@
 package screens
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 
@@ -12,9 +13,10 @@ import (
 	"gioui.org/widget/material"
 	"github.com/deoxyimran/gioui-livekit-client/ui/components"
 	"github.com/deoxyimran/gioui-livekit-client/ui/media/video"
-	"github.com/deoxyimran/gioui-livekit-client/ui/theme"
+	mytheme "github.com/deoxyimran/gioui-livekit-client/ui/theme"
 	"github.com/deoxyimran/gioui-livekit-client/ui/utils"
 	"github.com/oligo/gioview/menu"
+	"github.com/oligo/gioview/theme"
 )
 
 type JoinRoom struct {
@@ -35,6 +37,7 @@ func NewJoinRoomScreen(stateManager *StateManager) *JoinRoom {
 		stateManager:   stateManager,
 		th:             th,
 		vidCanvas:      components.NewVideoCanvas(&vs, image.Pt(380, 260)),
+		deviceSelector: newDevSelector(th, stateManager, []string{"mic1", "mic2"}, []string{"cam1", "cam2"}),
 		userNameEditor: newEditor(th),
 	}
 
@@ -73,22 +76,112 @@ func (e *editor) layout(gtx C) D {
 }
 
 type devSelector struct {
-	th          *material.Theme
-	camDropDown *menu.DropdownMenu
-	micDropDown *menu.DropdownMenu
-	micPaths    []string
-	camPaths    []string
-	w           int
+	th *material.Theme
+	st *StateManager
+
+	camDropdownTh        *theme.Theme
+	camDropdownClickable widget.Clickable
+	camDropdown          *menu.DropdownMenu
+
+	micDropdownTh        *theme.Theme
+	micDropdownClickable widget.Clickable
+	micDropdown          *menu.DropdownMenu
+
+	micPaths []string
+	camPaths []string
+	w        int
 }
 
-func newDevSelector(th *material.Theme, micPaths []string, camPaths []string) devSelector {
-	return devSelector{
+func newDevSelector(th *material.Theme, st *StateManager, micPaths []string, camPaths []string) devSelector {
+	d := devSelector{
 		th: th,
 	}
+	var camDropDown, micDropDown *menu.DropdownMenu
+	if camPaths != nil {
+		d.camPaths = camPaths
+		camDropDown = d.newCamDropdown()
+	}
+	if micPaths != nil {
+		d.micPaths = micPaths
+		micDropDown = d.newMicDropdown()
+	}
+	d.camDropdown = camDropDown
+	d.micDropdown = micDropDown
+
+	th_ := theme.NewTheme("./fonts", nil, false)
+	th_.TextSize = unit.Sp(14)
+	th_.Bg = color.NRGBA{255, 255, 255, 255}
+	th_.Bg = color.NRGBA{140, 140, 140, 255}
+	d.camDropdownTh = th_
+	d.micDropdownTh = th_
+
+	return d
+}
+
+func (d *devSelector) newCamDropdown() *menu.DropdownMenu {
+	options := [][]menu.MenuOption{}
+	options = append(options, []menu.MenuOption{})
+	for i, v := range d.camPaths {
+		options[0] = append(options[0], menu.MenuOption{
+			Layout: func(gtx menu.C, th *theme.Theme) menu.D {
+				lb := material.Label(th.Theme, unit.Sp(14), fmt.Sprint(v, i+1))
+				return lb.Layout(gtx)
+			},
+			OnClicked: func() error {
+				fmt.Println("Clicked cam", i+1)
+				return nil
+			},
+		})
+	}
+	return menu.NewDropdownMenu(options)
+}
+
+func (d *devSelector) newMicDropdown() *menu.DropdownMenu {
+	options := [][]menu.MenuOption{}
+	options = append(options, []menu.MenuOption{})
+	for i, v := range d.camPaths {
+		options[0] = append(options[0], menu.MenuOption{
+			Layout: func(gtx menu.C, th *theme.Theme) menu.D {
+				lb := material.Label(th.Theme, unit.Sp(14), fmt.Sprint(v, i+1))
+				return lb.Layout(gtx)
+			},
+			OnClicked: func() error {
+				fmt.Println("Clicked mic", i+1)
+				return nil
+			},
+		})
+	}
+	return menu.NewDropdownMenu(options)
 }
 
 func (d *devSelector) layout(gtx C) D {
-	return layout.Dimensions{}
+	dims := layout.Flex{
+		Axis: layout.Horizontal,
+	}.Layout(gtx,
+		layout.Flexed(1, func(gtx C) D {
+			d.camDropdown.Update(gtx)
+			if d.camDropdownClickable.Clicked(gtx) {
+				d.camDropdown.ToggleVisibility(gtx)
+			}
+			return layout.Center.Layout(gtx, func(gtx C) D {
+				dims := material.Button(d.th, &d.camDropdownClickable, "Choose Cam").Layout(gtx)
+				d.camDropdown.Layout(gtx, d.camDropdownTh)
+				return dims
+			})
+		}),
+		layout.Flexed(1, func(gtx C) D {
+			d.micDropdown.Update(gtx)
+			if d.micDropdownClickable.Clicked(gtx) {
+				d.micDropdown.ToggleVisibility(gtx)
+			}
+			return layout.Center.Layout(gtx, func(gtx C) D {
+				dims := material.Button(d.th, &d.micDropdownClickable, "Choose Mic").Layout(gtx)
+				d.micDropdown.Layout(gtx, d.micDropdownTh)
+				return dims
+			})
+		}),
+	)
+	return dims
 }
 
 func (j *JoinRoom) Layout(gtx C, screenPointer *Screen) D {
@@ -96,7 +189,7 @@ func (j *JoinRoom) Layout(gtx C, screenPointer *Screen) D {
 		// Fullscreen background
 		func(gtx C) D {
 			defer clip.Rect{Max: gtx.Constraints.Max}.Push(gtx.Ops).Pop()
-			color := theme.BackgroundColor()
+			color := mytheme.BackgroundColor()
 			paint.ColorOp{Color: color}.Add(gtx.Ops)
 			paint.PaintOp{}.Add(gtx.Ops)
 			return layout.Dimensions{Size: gtx.Constraints.Max}
@@ -118,8 +211,14 @@ func (j *JoinRoom) Layout(gtx C, screenPointer *Screen) D {
 											}.Layout(gtx,
 												// Video canvas
 												layout.Rigid(j.vidCanvas.Layout),
-												// Username editor
-												layout.Rigid(j.userNameEditor.layout),
+												// Device selector
+												layout.Rigid(func(gtx C) D {
+													// gtx.Constraints.Max.X, gtx.Constraints.Min.X = 600, 600
+													// gtx.Constraints.Max.Y, gtx.Constraints.Min.Y = 200, 200
+													return j.deviceSelector.layout(gtx)
+												}),
+												// // Username editor
+												// layout.Rigid(j.userNameEditor.layout),
 											)
 										},
 									)
